ba18aebf3aecd9c46095cb7e852a12599276afab
ushanil <ush.gopi@gmail.com>
2019-11-25 13:46:11 -0500

    done with lab

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/Franc.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/Franc.java
@@ -1,6 +1,16 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class Franc implements ConvertableCurrency {
+    @Override
+    public CurrencyType getType() {
+        return CurrencyType.FRANC;
+    }
+
+    @Override
+    public Double convert(CurrencyType currencyType) {
+        return (Double)currencyType.getRate()/this.getType().getRate();
+    }
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/USDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/USDollar.java
@@ -1,6 +1,16 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class USDollar implements ConvertableCurrency {
+    @Override
+    public Double convert(CurrencyType currencyType) {
+        return currencyType.getRate()/this.getType().getRate();
+    }
+
+    @Override
+    public CurrencyType getType() {
+        return CurrencyType.US_DOLLAR;
+    }
 }
